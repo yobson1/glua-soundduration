b165c9052eabe683a81613ec2ac72dcb7e06f4ff
Brad <bradleyglaurie@gmail.com>
2020-12-19 10:26:19 +0000

    Added sample sounds & tests

---
+++ lua/autorun/sounddurationtest.lua
@@ -0,0 +1,9 @@
+-- Sound duration ~132 seconds
+print("MP3")
+print(SoundDuration("sample.mp3")) -- 146.92733764648
+print(NewSoundDuration("sound/sample.mp3")) -- 132.23183673469
+
+-- Sound duration ~29 seconds
+print("WAV")
+print(SoundDuration("sample.wav")) -- 59.25732421875
+print(NewSoundDuration("sound/sample.wav")) -- 29.629943310658
